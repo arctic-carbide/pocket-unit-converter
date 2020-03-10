9fbe5347237d8eea887d83f8ea43dd3b10af2e5f
Myers <myersmk@UMICH.EDU>
2020-03-10 16:12:36 -0400

    initial commit

---
+++ app/src/main/java/umd/mad/frag/tempdist/MainActivity.java
@@ -0,0 +1,13 @@
+package umd.mad.frag.tempdist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
