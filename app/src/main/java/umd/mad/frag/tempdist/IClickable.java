b2a80c3f2939b0da7f8803714883eb8c55759002
Mitchell Myers <myersmk@umich.edu>
2020-03-18 23:58:03 -0400

    it finally works

--- app/src/main/java/umd/mad/frag/tempdist/IClickable.java
+++
@@ -1,7 +0,0 @@
-package umd.mad.frag.tempdist;
-
-public interface IClickable {
-    public void Convert() {
-
-    }
-}
--- app/src/main/java/umd/mad/frag/tempdist/TemperatureFragment.java
+++ app/src/main/java/umd/mad/frag/tempdist/TemperatureFragment.java
@@ -1,27 +1,16 @@
 package umd.mad.frag.tempdist;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TemperatureFragment extends Fragment implements IClickable {
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_convert_fahrenheit, container, false);
-
-
-
-        return view;
-    }
+public class TemperatureFragment extends Fragment {
 
     @Override
-    public void Convert() {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        return inflater.inflate(R.layout.fragment_convert_fahrenheit, container, false);
     }
 }
